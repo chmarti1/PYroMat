Passed test.py in python 3 and 2, re-organized unit conversion algorithms, corrected bug in IF97 _region(), corrected python3 bug in igmix

pyromat/registry/ig.py
@@ -16,7 +16,7 @@ methods and their unit conventions:
   d()  density          (unit_matter / unit_volume)
   e()  internal energy  (unit_energy / unit_matter)
   h()  enthalpy         (unit_energy / unit_matter)
-  k()  spec. heat ratio (dless)
+  gam()  spec. heat ratio (dless)
   mw() molecular weight (unit_mass / unit_molar)
   R()  gas constant     (unit_energy / unit_temperature / unit_matter)
   s()  entropy          (unit_energy / unit_temperature / unit_matter)
@@ -616,7 +616,7 @@ The following test criteria are used:
         units = ['J','mol','bar','K']
         R = pyro.units.const_Ru
         tag = '   8 %3s %3s %3s %3s'%tuple(units)
-        subresult &= _prop_test(tag, self.k, (Tref,), cpref/(cpref-R), units)
+        subresult &= _prop_test(tag, self.gam, (Tref,), cpref/(cpref-R), units)
 
         if report_level >= REP_MINIMAL:
             ff.write('Criterion 8: specific heat ratio')
@@ -775,8 +775,13 @@ Returns unit_energy / unit_matter / unit_temperature
         # Check for default values
         if T is None:
             T = pyro.config['def_T']
+        if not isinstance(T,np.ndarray):
+            T = np.array(T)
+
         if p is None:
             p = pyro.config['def_p']
+        if not isinstance(p,np.ndarray):
+            p = np.array(p)
 
         # Perform temperature conversion
         T = pyro.units.temperature_scale(T,to_units='K')
@@ -926,9 +931,9 @@ Returns unit_energy/unit_temperature/unit_matter
         R = pyro.units.matter(R, self.data['mw'], from_units='mol', exponent=-1)
         return R
 
-    def k(self,T=None,p=None):
+    def gam(self,T=None,p=None):
         """Specific heat ratio
-    k(T,p)
+    gam(T,p)
 Both arguments are optional, and will default to 'def_T' and 'def_p'
 configuration parameters if they are left undefined.  Ideal gas specific 
 heat ratio is not actually a function of p, but it is permitted as an 

pyromat/registry/igmix.py
@@ -20,7 +20,7 @@ IGMIX objects offer the following property functions:
   d()  density          (unit_matter / unit_volume)
   e()  internal energy  (unit_energy / unit_matter)
   h()  enthalpy         (unit_energy / unit_matter)
-  k()  spec. heat ratio (dless)
+  gam()  spec. heat ratio (dless)
   mw() molecular weight (unit_mass / unit_molar)
   R()  gas constant     (unit_energy / unit_temperature / unit_matter)
   s()  entropy          (unit_energy / unit_temperature / unit_matter)
@@ -44,16 +44,87 @@ temperature intervals of the constituents.
         # Initialize the static molar and mass fractions
         self._x = None
         self._y = None
+      
 
-        # Define inverstion routines
-        self.T_h = pyro.solve.solve1n('T',
-            f=self.h, df=self.cp, param_init=1000.)
 
-        def ds(T,p=None):
-            return self.cp(T,p)/T 
+    def _invT(self, value, prop, p=None):
+        """Inverse property function
+    T = _invT(value, prop, dprop, p=None)
 
-        self.T_s = pyro.solve.solve1n('T',
-            f=self.s, df=ds, param_init=1000.)        
+This is an internal service method used to provide T_s and T_h.
+Iterate on values of temperature so that the property method, prop,
+returns value.
+
+The algorithm uses a modified Newton iteration.  The property derivative
+is approximated numerically instead of relying on explicit derivatives.
+The numerical cost of explicitly evaluating a function's derivatives is 
+approximately equal to numerical approximation in 1D, but this 
+drastically simplifies the implementation.
+
+value   the property value to obtain
+prop    the property method to be inverted
+p       the pressure
+"""
+        if p is None:
+            p = pyro.config['def_p']
+
+        # Generic iteration parameters
+        N = 100 # Maximum iterations
+        small = 1e-8    # A "small" number
+        epsilon = 1e-6  # Iteration precision
+
+        Tmin,Tmax = self.Tlim()
+
+        it = np.nditer((None,value,p),op_flags=[['readwrite','allocate'],['readonly','copy'],['readonly','copy']],op_dtypes='float')
+        for T_,y_,p_ in it:
+            # Use Tk as the iteration parameter.  We will write to T_ later.
+            # Initialize it to be in the center of the species' legal range.
+            Tk = 0.5*(Tmin + Tmax)
+            Tk1 = Tk
+            # Initialize dT - the change in T
+            dT = 0.
+            # Calculate an error threshold
+            thresh = max(small, abs(epsilon * y_))
+            # Initialize absek1 - the absolute error from the last iteration
+            #    Using +infty will force the error reduction criterion to be met
+            abs_ek1 = float('inf')
+            fail = True
+            for count in range(N):
+                ## CALL THE PROPERTY FUNCTION ##
+                yk = prop(T=Tk,p=p_)
+                # Calculate error
+                ek = yk-y_
+                abs_ek = abs(ek)
+                # Test for convergence
+                if abs_ek < thresh:
+                    T_[...] = Tk
+                    fail = False
+                    break
+                # If the error did not reduce from the last iteration
+                elif abs_ek > abs_ek1:
+                    dT /= 2.
+                    Tk = Tk1 + dT
+                # Continue normal iteration
+                else:
+                    # Shift out the old values
+                    abs_ek1 = abs_ek
+                    Tk1 = Tk
+                    ## ESTIMATE THE DERIVATIVE ##
+                    dT = max(small, epsilon*Tk)    # Make a small perturbation
+                    dydx = (prop(T=Tk+dT,p=p_) - yk)/dT
+                    # Calculate the next step size
+                    dT = -ek / dydx
+                    # Produce a tentative next value
+                    Tk = Tk1 + dT
+                    # Test the guess for containment in the temperature limits
+                    # Shrink the increment until Tk is contained
+                    while Tk<Tmin or Tk>Tmax:
+                        dT /= 2.
+                        Tk = Tk1 + dT
+            if fail:
+                raise pyro.utility.PMAnalysisError('_invT() failed to converge!')
+        return it.operands[0]
+
 
     def Tlim(self):
         """Temperature limits
@@ -83,7 +154,7 @@ Returns unit_temperature
         # Otherwise, weight by mole fraction
         else:
             W = self.X()
-        for ss,f in W.iteritems():
+        for ss,f in W.items():
             out += f*pyro.dat.data[ss].cp(T,p)
         return out
 
@@ -96,7 +167,7 @@ Returns unit_temperature
         # Otherwise, weight by mole fraction
         else:
             W = self.X()
-        for ss,f in W.iteritems():
+        for ss,f in W.items():
             out += f*pyro.dat.data[ss].cv(T,p)
         return out
 
@@ -117,7 +188,7 @@ Returns unit_temperature
         # Otherwise, weight by mole fraction
         else:
             W = self.X()
-        for ss,f in W.iteritems():
+        for ss,f in W.items():
             out += f*pyro.dat.data[ss].h(T,p,hf)
         return out
 
@@ -130,7 +201,7 @@ Returns unit_temperature
         # Otherwise, weight by mole fraction
         else:
             W = self.X()
-        for ss,f in W.iteritems():
+        for ss,f in W.items():
             out += f*pyro.dat.data[ss].e(T,p)
         return out
 
@@ -151,11 +222,11 @@ Returns unit_temperature
         # Otherwise, weight by mole fraction
         else:
             W = self.X()
-        for ss,f in W.iteritems():
+        for ss,f in W.items():
             out += f*pyro.dat.data[ss].s(T,p)
         return out
 
-    def k(self,T=None,p=None):
+    def gam(self,T=None,p=None):
         """Specific heat ratio"""
         return self.cp(T,p) / self.cv(T,p)
 
@@ -174,7 +245,7 @@ in the mixture."""
             # If the species is defined by mass (and not by mole)
             if self.data['bymass']:
                 N = 0.  # Mole count
-                for key,value in self.data['contents'].iteritems():
+                for key,value in self.data['contents'].items():
                     temp = value / pyro.dat.data[key].data['mw']
                     # Add str() to convert out of unicode
                     self._x[str(key)] = temp
@@ -182,7 +253,7 @@ in the mixture."""
             # If the species is defined by mole
             else:
                 N = 0.  # Mole count
-                for key,value in self.data['contents'].iteritems():
+                for key,value in self.data['contents'].items():
                     # Add str() to convert out of unicode
                     self._x[str(key)] = value
                     N += value
@@ -208,14 +279,14 @@ in the mixture."""
             # If the species is defined by mass (and not by mole)
             if self.data['bymass']:
                 M = 0.  # Mass count
-                for key,value in self.data['contents'].iteritems():
+                for key,value in self.data['contents'].items():
                     # Add str() to convert out of unicode
                     self._y[str(key)] = value
                     M += value
             # If the species is defined by mole
             else:
                 M = 0.  # Mole count
-                for key,value in self.data['contents'].iteritems():
+                for key,value in self.data['contents'].items():
                     temp = value * pyro.dat.data[key].data['mw']
                     # Add str() to convert out of unicode
                     self._y[str(key)] = temp
@@ -227,4 +298,31 @@ in the mixture."""
         # Return the dictionary
         return self._y
 
+
+    def T_s(self,s,p=None):
+        """Temperature as a function of entropy
+    T = T_s(s)
+        or
+    T = T_s(s,p)
+
+Accepts unit_energy / unit_matter / unit_temperature
+        unit_pressure
+Returns unit_temperature
+"""
+        return self._invT(s, self.s, p)
+
+
+    def T_h(self,h,p=None):
+        """Temperature as a function of enthalpy
+    T = T_h(h)
+        or
+    T = T_h(h,p)
+
+Returns the temperature as a function of enthalpy and pressure
+
+Accepts unit_energy / unit_matter / unit_temperature
+        unit_pressure
+Returns unit_temperature
+"""
+        return self._invT(h, self.h, p)
 

pyromat/units.py
@@ -121,7 +121,8 @@ new system of units.
         conv = self.table[to_units] / self.table[from_units]
         if exponent:
             conv **= exponent
-        return value * conv
+
+        return np.array(value) * conv
 
     def __getitem__(self,item):
         return self.table.__getitem__(item)
@@ -402,24 +403,27 @@ original default scale.
         else:
             return value
 
+    # copy the input
+    out = np.array(value)
+
     if from_units == 'C':
-        value += 273.15
+        out += 273.15
     elif from_units == 'R':
-        value /= 1.8
+        out /= 1.8
     elif from_units == 'F':
-        value = value/1.8 + 255.3722222222222
+        out = out/1.8 + 255.3722222222222
     elif from_units == 'eV':
-        value *= 11604.521662304598
+        out *= 11604.521662304598
 
     if to_units == 'C':
-        value -= 273.15
+        out -= 273.15
     elif to_units == 'R':
-        value *= 1.8
+        out *= 1.8
     elif to_units == 'F':
-        value = (value - 255.3722222222222)*1.8
+        out = (out - 255.3722222222222)*1.8
     elif to_units == 'eV':
-        value /= 11604.521662304598
-    return value
+        out /= 11604.521662304598
+    return out
 
 # Validated 11/18/2017
 def gauge_to_abs(value, units=None, patm=const_pstd):
@@ -434,7 +438,7 @@ default, the standard pressure is used.  If the atmospheric pressure
 is already known in the same units as the gauge pressure, do not use
 gauge_to_absolute(); simply add it to the old value.
 """
-    return value + pressure(patm,from_units='bar',to_units=units)
+    return np.array(value) + pressure(patm,from_units='bar',to_units=units)
 
 # Validated 11/18/2017
 def abs_to_gauge(value, units=None, patm=const_pstd):
@@ -449,7 +453,7 @@ default, the standard pressure is used.  If the atmospheric pressure
 is already known in the same units as the gauge pressure, do not use
 gauge_to_absolute(); simply add it to the old value.
 """
-    return value - pressure(patm,from_units='bar',to_units=units)
+    return np.array(value) - pressure(patm,from_units='bar',to_units=units)
 
 # Validated 11/18/2017
 def matter(value, mw, from_units=None, to_units=None, exponent=None):
@@ -488,7 +492,9 @@ If the from_units or the to_units values are not specified, the pyromat
             conv = mass[to_units] * mw / molar[from_units]
     if exponent:
         conv**=exponent
-    return value * conv
+
+    out = np.array(value) * conv
+    return out
 
 
 def show():

test.py
@@ -112,7 +112,7 @@ with open('test.log','w+') as writeto:
         writeto.write("  {0:s} {2:s} {4:s}\n".format(*os.uname()))
     writeto.write('\n')
 
-    # Test the igfit class
+    # Test the ig class
     T = 500.
     p = 20.
     args = {'T':T, 'p':p}
@@ -123,21 +123,7 @@ with open('test.log','w+') as writeto:
         's':220.693/mw-R*np.log(p/1.), 'd':p*1e2/R/T }
     writeto.write("Diatomic oxygen tabulated reference values found\n" + 
     "http://kinetics.nist.gov/janaf/html/O-029.html\n")
-    runargtest(writeto,'O2',args,reference)
-
-
-    # Test the igtab class
-    T = 600.
-    p = 20.
-    args = {'T':T, 'p':p}
-    # Reference values from the NIST tables
-    mw = 253.80894
-    R = 8.314 / mw
-    reference = {'mw':mw, 'R':R, 'cp':37.612 / mw, 'h':73.690*1000./mw, 
-        's':286.764/mw - R*np.log(p/1.), 'd':p*1e2/R/T }
-    writeto.write("Diatomic iodine tabulated reference values found\n" + 
-    "http://kinetics.nist.gov/janaf/html/I-027.html\n")
-    runargtest(writeto,'I2',args,reference)
+    runargtest(writeto,'ig.O2',args,reference)
 
     # Test the mixture class
     T = [320., 1000., 1000.]
@@ -149,7 +135,7 @@ with open('test.log','w+') as writeto:
         " Chemistry and\nPhysics 97th Edition ``Thermophysical Properties of" +
         " Air''\nby Eric W. Lemon.\n"+
         "Enthalpy and Entropy values were adjusted to match a 1 bar and 300K.\n")
-    air = pyro.get('air')
+    air = pyro.get('ig.air')
     h = air.h(T=T[0],p=1.) - h[0] + np.array(h)
     s = air.s(T=T[0],p=1.) - s[0] + np.array(s)
     reference = {'cp':cp, 's':s, 'h':h}
@@ -180,7 +166,7 @@ with open('test.log','w+') as writeto:
     writeto.write( "Steam validation values for region 1 found at \n" +
     "http://www.iapws.org/relguide/IF97-Rev.pdf\n" +
     "   Table 5 pg 9\n" )
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
     T = np.array([300., 700., 700.])
     p = np.array([.035, .035, 300.])
@@ -197,7 +183,7 @@ with open('test.log','w+') as writeto:
     writeto.write( "Steam validation values for region 2 found at \n" +
     "http://www.iapws.org/relguide/IF97-Rev.pdf\n" +
     "   Table 15 pg 17\n" )
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
     T = np.array([650., 650., 750.])
     p = np.array([.255837018e3, .222930643e3, .783095639e3])
@@ -214,7 +200,7 @@ with open('test.log','w+') as writeto:
     writeto.write( "Steam validation values for region 3 found at \n" +
     "http://www.iapws.org/relguide/IF97-Rev.pdf\n" +
     "   Table 33 pg 32\n" )
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
     T = np.array([1500., 1500., 2000.])
     p = np.array([5., 300., 300.])
@@ -231,10 +217,10 @@ with open('test.log','w+') as writeto:
     writeto.write( "Steam validation values for region 5 found at \n" +
     "http://www.iapws.org/relguide/IF97-Rev.pdf\n" +
     "   Table 42 pg 40\n" )
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
 
-    test = pyro.get('steam')
+    test = pyro.get('mp.H2O')
     T = np.array([300., 500., 600.])
     args = {'T':T}
     reference = {'ps':np.array([.353658941e-1, .263889776e2, .123443146e3])}
@@ -242,12 +228,12 @@ with open('test.log','w+') as writeto:
     writeto.write( "Steam validation values for saturation (region 4) found at \n" +
     "http://www.iapws.org/relguide/IF97-Rev.pdf\n" +
     "   Table 35 pg 34 and Table 36 pg 36\n" )
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
     p = np.array([1., 10., 100.])
     args = {'p':p}
     reference = {'Ts': np.array([.372755919e3, .453035632e3, .584149488e3])}
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
 
     # Inverse relations
@@ -281,9 +267,9 @@ with open('test.log','w+') as writeto:
 
     reference = {'T_h':T}
     args = {'h':h, 'p':p}
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 
     reference = {'T_s':T}
     args = {'s':s, 'p':p}
-    runargtest(writeto,'steam',args,reference)
+    runargtest(writeto,'mp.H2O',args,reference)
 