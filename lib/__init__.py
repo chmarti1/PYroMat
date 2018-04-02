663dbdea99c81b16498687cbf965c0c6aaedcbc6
Christopher Martin <crm28@psu.edu>
2018-04-02 12:43:34 -0400

    Corrected configuration __repr__() and if97 hsd() bugs

--- lib/__init__.py
+++ lib/__init__.py
@@ -38,7 +38,7 @@ function,
 # utility.load_config() checks this value to establish the read-only version
 # setup.py looks for this line to establish the version at install
 # MUST be unindented
-__version__ = "2.0.4"
+__version__ = "2.0.6"
 
 
 # loading the PYroMat utility functions
