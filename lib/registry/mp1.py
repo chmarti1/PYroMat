Fixing a bug in mp1._argparse. When converting p to the correct units, it was not applied to the value of p. Added test cases in steammp1test.py that would demonstrate the bug prior to fixing.

lib/registry/mp1.py
@@ -1524,7 +1524,7 @@ other conditions, x<0 and d1 == d2.
         # If p is the primary parameter
         elif p is not None:
             # Convert p to the correct units
-            pm.units.pressure(
+            p = pm.units.pressure(
                     np.asarray(p, dtype=float), 
                     to_units='Pa')
             if p.ndim==0: